007a9ca7caca5bc13c878314cf68d94f18a76c96
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2022-08-31 21:21:56 +0200

    moving book component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,10 @@ import AppContainer from "./AppContainer";
 import AppContact from "./AppContact";
 import AppExperience from "./AppExperience";
 import AppGraphic from "./AppGraphic";
-import AppBook from "./AppBook";
 
 function App() {
   return (
     <div className="app">
-      <AppBook />
       <AppContainer
         items={
           <>
