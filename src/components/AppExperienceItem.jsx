1ad98d8bda5135df7313c7692037157faf1694f6
Maciej Spiechowicz <maciej.spiechowicz@cityos.pl>
2022-09-23 21:47:13 +0200

    Adding color map

--- src/components/AppExperienceItem.jsx
+++ src/components/AppExperienceItem.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// eslint-disable-next-line no-undef
+const definedColorMap = new Map();
+definedColorMap.set("red", "text-red");
+definedColorMap.set("blue", "text-blue");
+definedColorMap.set("yellow", "text-yellow");
+definedColorMap.set("green", "text-green");
+
 function AppExperienceItem({ title, description, color }) {
+  const getStyle = () => {
+    if (definedColorMap.has(color)) {
+      return definedColorMap.get(color);
+    }
+    return "text-white";
+  };
+
   return (
     <div className="flex flex-col">
-      <h3 className={`text-2xl md:text-4xl font-bold
-        ${color === 'red' && 'text-red'}
-        ${color === 'blue' && 'text-blue'}
-        ${color === 'yellow' && 'text-yellow'}
-        ${color === 'green' && 'text-green'}
-      `}>
+      <h3 className={`text-2xl md:text-4xl font-bold ${getStyle()}`}>
         {title}
       </h3>
       <p className="text-sm md:text-base">
