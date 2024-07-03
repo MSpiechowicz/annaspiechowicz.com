40771a615b8f6978d4470772a40cc56bf50da2d8
Maciej Spiechowicz <spm1fr@bosch.com>
2024-07-03 13:27:03 +0200

    adjust the styles for the mobile view, remove the graphics from the smaller devices

--- src/components/AppExperience.jsx
+++ src/components/AppExperience.jsx
@@ -3,7 +3,7 @@ import AppExperienceItem from "./AppExperienceItem";
 
 function AppExperience() {
   return (
-    <div className="grid grid-cols-3 gap-4 lg:gap-0 mt-6 md:mt-10 max-w-lg lg:max-w-lg">
+    <div className="grid grid-cols-3 gap-4 lg:gap-0 mt-8 md:mt-10 max-w-lg lg:max-w-lg">
       <AppExperienceItem
         title="7+"
         description="Years of Experience"
