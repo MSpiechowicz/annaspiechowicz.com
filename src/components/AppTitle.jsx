22ee98eb33bd52b33f3edbfbceffca47eb5622ed
Maciej Spiechowicz <spm1fr@bosch.com>
2024-07-03 13:27:03 +0200

    adjust the styles for the mobile view, remove the graphics from the smaller devices

--- src/components/AppTitle.jsx
+++ src/components/AppTitle.jsx
@@ -5,11 +5,11 @@ function AppTitle() {
   return (
     <div className="flex flex-col gap-2 md:gap-4 static">
       <AppBook />
-      <div className="text-4xl md:text-6xl lg:text-8xl font-bold flex flex-col gap-1">
+      <div className="text-4xl md:text-6xl font-bold flex flex-col gap-1">
         <h1>Anna</h1>
         <h1>Spiechowicz</h1>
       </div>
-      <div className="text-2xl md:text-4xl lg:text-6xl font-medium flex flex-row gap-2 md:gap-3 lg:gap-4">
+      <div className="text-2xl md:text-4xl font-medium flex flex-row gap-2 md:gap-3 lg:gap-4">
         <h2>Business</h2>
         <h2>Analyst</h2>
       </div>
